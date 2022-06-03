2d541f8594193e1cbc0b24fcdb8ebea9410d3468
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2022-06-03 18:52:18 +0100

    v + h central

--- index.php
+++ index.php
@@ -11,23 +11,33 @@
                 font-style: normal;
                 font-weight: 400;
                 src: url("https://jahidulpabelislam.com/assets/webfonts/cabin-latin-regular.eot?v=14"); // IE9 Compat Modes
-            src: local("Cabin"),
-            local("Cabin-Regular"),
-            url("https://jahidulpabelislam.com/assets/webfonts/cabin-latin-regular.eot?v=14#iefix") format("embedded-opentype"), // IE6-IE8
-            url("https://jahidulpabelislam.com/assets/webfonts/cabin-latin-regular.woff2?v=14") format("woff2"), // Super Modern Browsers
-            url("https://jahidulpabelislam.com/assets/webfonts/cabin-latin-regular.woff?v=14") format("woff"), // Modern Browsers
-            url("https://jahidulpabelislam.com/assets/webfonts/cabin-latin-regular.ttf?v=14") format("truetype"), // Safari, Android, iOS
-            url("https://jahidulpabelislam.com/assets/webfonts/cabin-latin-regular.svg?v=14#Cabin") format("svg"); // Legacy iOS
-            unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+                src: local("Cabin"),
+                    local("Cabin-Regular"),
+                    url("https://jahidulpabelislam.com/assets/webfonts/cabin-latin-regular.eot?v=14#iefix") format("embedded-opentype"), // IE6-IE8
+                    url("https://jahidulpabelislam.com/assets/webfonts/cabin-latin-regular.woff2?v=14") format("woff2"), // Super Modern Browsers
+                    url("https://jahidulpabelislam.com/assets/webfonts/cabin-latin-regular.woff?v=14") format("woff"), // Modern Browsers
+                    url("https://jahidulpabelislam.com/assets/webfonts/cabin-latin-regular.ttf?v=14") format("truetype"), // Safari, Android, iOS
+                    url("https://jahidulpabelislam.com/assets/webfonts/cabin-latin-regular.svg?v=14#Cabin") format("svg"); // Legacy iOS
+                    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD
+                ;
             }
 
             h1 {
                 font-family: Cabin, sans-serif;
                 color: #0375b4;
             }
-</style>
+        </style>
     </head>
-    <body>
-        <h1 style="">COMING SOON!</h1>
+    <body
+        style="
+            height: 100vh;
+            padding: 0;
+            margin: 0;
+            display: flex;
+            align-items: center;
+            text-align: center;
+        "
+    >
+        <h1 style="width: 100%;">COMING SOON!</h1>
     </body>
 </html>
